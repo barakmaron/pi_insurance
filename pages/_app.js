9acfeabd5b9db1c00052febe1b29385f6e21c0c9
Barak Maron <Barak062@gmail.com>
2022-09-21 13:14:33 +0300

    feat: add google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,21 @@
+import Script from 'next/script';
 import { AppWrapper } from '../Hooks/useAppContext';
 import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return <AppWrapper>
+    <Script strategy="lazyOnload" src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`} />
+
+    <Script strategy="lazyOnload" id='google'>
+        {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+            page_path: window.location.pathname,
+            });
+        `}
+    </Script>
     <Component {...pageProps} />
   </AppWrapper>;
 }
