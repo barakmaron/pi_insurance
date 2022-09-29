c58e73c74b112938a803a1f9a4c95a6228d0fd9e
Barak Maron <Barak062@gmail.com>
2022-09-29 18:18:04 +0300

    fix: revalidate

--- pages/api/revalidate.js
+++ pages/api/revalidate.js
@@ -1,3 +1,4 @@
+import blogsDB from "../../services/db/blogs";
 
 export default function handler(req, res) {
     const { method } = req;
@@ -16,8 +17,11 @@ async function Validate(req, res) {
     if(secret !== process.env.NEXT_PUBLIC_REVALIDATE_SECRET)
         return res.status(401).json();
     try {
-        await res.revalidate('/blogs');
-        await res.revalidate('/admin/blogs');
+        const blogs = res.revalidate('/blogs');
+        const admin_blogs = res.revalidate('/admin/blogs');
+        const blogs_array = await blogsDB.GetAll();
+        const blogs_promise = [...blogs_array.map(blog => [res.revalidate(`/blogs/${blog.id}`), res.revalidate(`/admin/blogs/${blog.id}`)])];
+        await Promise.all([blogs, admin_blogs, ...blogs_promise]);
         return res.status(200).json({ revalidate: true });
     } catch (err) {
         return res.status(500).json(err);
